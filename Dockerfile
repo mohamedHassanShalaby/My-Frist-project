686f9ccddce93eb70863fe2524d997a1a0c2821b
mohamedHassanShalaby <mh0337257@gmail.com>
2023-11-16 19:00:56 +0200

    mm

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FORM openjdk
+
+WORKDIR / application
+
+COPY MyName.java .
+
+RUN javac MyName.java
+
+CMD java MyName
---
+++ MyName.java
@@ -0,0 +1,8 @@
+package com.mycompany.myname;
+
+public class MyName {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Mohamed!");
+    }
+}
